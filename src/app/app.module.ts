2a24abd316f8e6528c5716be397a6026940d81a5
dbkira96 <dbkira96@gmail.com>
2020-12-15 19:12:08 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+import {InputTextModule} from 'primeng/inputtext';
+import {PasswordModule} from 'primeng/password';
+import {ToastModule} from 'primeng/toast';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TableModule} from 'primeng/table';
+import {FieldsetModule} from 'primeng/fieldset';
+import {TabMenuModule} from 'primeng/tabmenu';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      LoginComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ButtonModule,
+    CardModule,
+    InputTextModule,
+    PasswordModule,
+    ToastModule,
+    TabMenuModule,
+    BrowserAnimationsModule,
+    TableModule,
+    FieldsetModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import {MessageService} from 'primeng/api';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  providers:[MessageService]
+})
+export class LoginComponent implements OnInit {
+
+  constructor(
+    private route: Router,
+    private authService: AuthService,
+    private messageService: MessageService
+  ) { }
+  
+    username: string = "";
+    password: string = "";
+
+    isAuthenticate: Boolean = false;
+
+    ngOnInit() {
+    }
+    doLogin() {
+      this.authService.authenticate(this.username, this.password).subscribe(
+        response => {
+          
+          if(response==true) {
+            sessionStorage.setItem("user", this.username);
+            this.isAuthenticate = true;
+            this.route.navigate(['home', this.username]);
+        }
+        else {
+            this.isAuthenticate = false;
+            this.messageService.add({key: 'tc', severity:'error', summary: 'Error', detail: 'Bad username or password'});
+          }
+        }
+      )
+    }
+}
